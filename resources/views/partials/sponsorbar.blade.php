7ecbb168d7fca95ae6048de0aaa3e247facf4a70
Chris <lemmer.j.christopher@gmail.com>
2021-01-12 13:28:23 -0600

    sponsorbar updated

--- resources/views/partials/sponsorbar.blade.php
+++ resources/views/partials/sponsorbar.blade.php
@@ -3,19 +3,19 @@
 
        <div class="flex">
 
-            <div class="flex-1 grid items-center">
+            <div class="flex-1">
                 <div class="flex justify-center">
                     <img src="{{ asset("assets/img/clients/AmericanSurgicalPros.png") }}" alt=""> 
                 </div>
             </div>
        
-            <div class="flex-1 grid items-center">
+            <div class="flex-1">
                 <div class="flex justify-center">
                     <img src="{{ asset("assets/img/clients/digicert.png") }}" alt=""> 
                 </div>
             </div>
 
-            <div class="flex-1 grid items-center">
+            <div class="flex-1">
                 <div class="flex justify-center">
                     <img src="{{ asset("assets/img/clients/ICE.png") }}" alt=""> 
                 </div>
